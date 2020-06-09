004608c1acabe7f6e47e699a61410967625a0647
Sean Stolberg <seans@unity3d.com>
2020-06-09 15:35:23 -0700

    add job link

--- Editor/CustomMetadataManager.cs
+++ Editor/CustomMetadataManager.cs
@@ -33,8 +33,7 @@ namespace com.unity.test.metadatamanager
                 new KeyValuePair<string, string>("testsrev", settings.TestsRevision),
                 new KeyValuePair<string, string>("testsrevdate", settings.TestsRevisionDate),
                 new KeyValuePair<string, string>("dependencies", string.Join(",", dependencies)),
-				new KeyValuePair<string, string>("MtRendering", string.Join(",", settings.MtRendering.ToString())),
-                new KeyValuePair<string, string>("GraphicsJobs", string.Join(",", settings.GraphicsJobs.ToString()))
+				new KeyValuePair<string, string>("joblink", string.Join(",", settings.JobLink)),
             };
             AppendMetadata(keyValuePairs);
             return metadata.Remove(0,1).ToString();
